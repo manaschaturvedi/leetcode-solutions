a1238aa9a265f6491c62a6f8a4c7667f511cb0e4
manas <manas.oid@gmail.com>
2019-08-06 00:49:34 +0530

    remove outer parantheses problem solution

---
+++ remove_outermost_parantheses.py
@@ -0,0 +1,69 @@
+'''
+Link: https://leetcode.com/problems/remove-outermost-parentheses/
+
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+
+Note:
+S.length <= 10000
+S[i] is "(" or ")"
+S is a valid parentheses string
+'''
+
+class Solution:
+    def removeOuterParentheses(self, S):
+        parans = []
+        start_afresh = True
+        for char in S:
+            if start_afresh:
+                current_paran = ''
+                openings, closings = 0, 0
+                if char == '(':
+                    openings += 1
+                elif char == ')':
+                    closings += 1
+                current_paran += char
+                start_afresh = False
+            else:
+                if char == '(':
+                    openings += 1
+                elif char == ')':
+                    closings += 1
+                current_paran += char
+                if openings == closings:
+                    parans.append(current_paran)
+                    start_afresh = True
+        final = ''
+        for elem in parans:
+            final += elem[1:-1]
+        return final
+
+sol = Solution()
+print(sol.removeOuterParentheses("(()())(())"))  # Output: ()()()
